541ae093ba95d65ed2348c3d8a47957035402d8d
Gabriel <tetradog@gmail.com>
2017-05-25 23:18:10 +0200

    subir archivos

---
+++ PokemonErranteGBA/ScriptEditor.xaml.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: tetra
+ * Fecha: 25/05/2017
+ * Hora: 22:54
+ * Licencia GNU GPL V3
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace PokemonErranteGBA
+{
+	/// <summary>
+	/// Interaction logic for ScriptEditor.xaml
+	/// </summary>
+	public partial class ScriptEditor : UserControl
+	{
+		public ScriptEditor()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ PokemonErranteGBA/Window1.xaml.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: tetra
+ * Fecha: 25/05/2017
+ * Hora: 22:45
+ * Licencia GNU GPL V3
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace PokemonErranteGBA
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class Window1 : Window
+	{
+		public Window1()
+		{
+			InitializeComponent();
+		}
+		void MiCargar_Click(object sender, RoutedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+		void MiSobre_Click(object sender, RoutedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+		void CmbPokedex_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
